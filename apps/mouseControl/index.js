d22cd59ce317124d21fbd678e65c5933edcfb8d1
k <xingya.shi@gmail.com>
2018-08-20 14:33:40 +0800

    更换matrix计算引擎，修改bug

--- apps/mouseControl/index.js
+++ apps/mouseControl/index.js
@@ -1,4 +1,5 @@
-import { mat4, vec3 } from 'gl-matrix';
+// import { mat4, vec3 } from 'gl-matrix';
+import { Matrix4 } from '../../utils/gl-matrix';
 
 const VSHADER_SOURCE = `
     attribute vec4 a_Position;
@@ -66,32 +67,41 @@ function main() {
         console.log('Failed to get the uniform location');
         return;
     }
-    let viewProjMatrix = mat4.create();
-    mat4.perspective(viewProjMatrix, 30.0, canvas.width / canvas.height, 1.0, 100.0);
-    let eye = vec3.fromValues(3.0, 3.0, 7.0);
-    let center = vec3.fromValues(0.0, 0.0, 0.0);
-    let up = vec3.fromValues(0.0, 1.0, 0.0);  //y轴正方向
-    mat4.lookAt(viewProjMatrix, eye, center, up);
+    // let viewProjMatrix = mat4.create();
+    // let viewProjMatrix = mat4.fromValues(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+
+    // viewProjMatrix = mat4.perspective(viewProjMatrix, 30.0 * Math.PI / 180.0, canvas.width / canvas.height, 1.0, 100.0);
+    // let eye = vec3.fromValues(3.0, 3.0, 7.0);
+    // let center = vec3.fromValues(0.0, 0.0, 0.0);
+    // let up = vec3.fromValues(0.0, 1.0, 0.0);  //y轴正方向
+    // viewProjMatrix = mat4.add(viewProjMatrix, viewProjMatrix, mat4.lookAt(mat4.fromValues(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), eye, center, up));
+
+    let viewProjMatrix = new Matrix4();
+
+    viewProjMatrix.setPerspective(30.0, canvas.width / canvas.height, 1.0, 100.0);
+
+
+    viewProjMatrix.lookAt(3.0, 3.0, 7.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
     let currentAngle = [0.0, 0.0];
     createEventHandlers(canvas, currentAngle);
 
-    if (!createTextures) {
+    if (!createTextures(gl)) {
         console.log('Failed tp initial the textures');
         return;
     }
 
     const tick = function () {
         draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
-        requestAnimationFrame(function () {
-            draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
-        }, canvas);
+        requestAnimationFrame(tick, canvas);
     };
     tick();
 
 }
 
-let g_mvpMatrix = mat4.create();
+// let g_mvpMatrix = mat4.create();
+// let g_mvpMatrix = mat4.fromValues(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+let g_mvpMatrix = new Matrix4();
 
 /**
  *
@@ -103,18 +113,27 @@ let g_mvpMatrix = mat4.create();
  * @param {*} currentAngle
  */
 function draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle) {
-    g_mvpMatrix = mat4.clone(viewProjMatrix);
-    mat4.fromRotation(g_mvpMatrix, currentAngle[0], vec3.fromValues(1.0, 0.0, 0.0));
-    mat4.fromRotation(g_mvpMatrix, currentAngle[1], vec3.fromValues(0.0, 1.0, 0.0));
+    // g_mvpMatrix = mat4.clone(viewProjMatrix);
+    // // console.log(viewProjMatrix);
+    // mat4.fromRotation(g_mvpMatrix, currentAngle[0], vec3.fromValues(1.0, 0.0, 0.0));
+    // mat4.fromRotation(g_mvpMatrix, currentAngle[1], vec3.fromValues(0.0, 1.0, 0.0));
+
+    g_mvpMatrix.set(viewProjMatrix);
+    // console.log(viewProjMatrix);
+    g_mvpMatrix.rotate(currentAngle[0], 1.0, 0.0, 0.0); // Rotation around x-axis
+    g_mvpMatrix.rotate(currentAngle[1], 0.0, 1.0, 0.0); // Rotation around y-axis
+
+
     //将`g_mvpMatrix`指定给`u_MvpMatrix`;
-    gl.uniformMatrix4fv(u_MvpMatrix, false, g_mvpMatrix);
+    gl.uniformMatrix4fv(u_MvpMatrix, false, g_mvpMatrix.elements);
+
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 }
 
 function createTextures(gl) {
     //创建纹理对象
-    const texture = gl.createTextures();
+    const texture = gl.createTexture();
     if (!texture) {
         console.log('Failed to create texture');
         return;
@@ -154,6 +173,10 @@ function loadTexture(gl, texture, u_Sampler, image) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     //配置纹理参数 
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    //设置图形纹理填充为水平、垂直拉伸。解决图片分辨率不属于2的幂数
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
     //配置纹理图像
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
     //将0号纹理单元传递给着色器中的取样器变量
@@ -169,7 +192,7 @@ function createEventHandlers(canvas, currentAngle) {
         const x = evt.clientX;
         const y = evt.clientY;
         const rect = canvas.getBoundingClientRect();
-        if (rect.left <= x && rect.right > x && rect.top <= y && rect.bottom > y) {
+        if (rect.left <= x && x < rect.right && rect.top <= y && y < rect.bottom) {
             lastX = x;
             lastY = y;
             dragging = true;
@@ -207,24 +230,24 @@ function createVertexBuffer(gl) {
     //  v2 ------ v3
     // front -> right -> up -> left -> down -> back
     const vertices = new Float32Array([
-        1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0,  //v0-v1-v2-v3 
-        1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0,// v0-v3-v4-v5
-        1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,  // v0-v5-v6-v1
-        - 1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0,//v1-v6-v7-v2
-        - 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, //v7-v4-v3-v2
-        1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0 //v4-v7-v6-v5
+        1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0,    // v0-v1-v2-v3 front
+        1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0, -1.0,    // v0-v3-v4-v5 right
+        1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, 1.0,    // v0-v5-v6-v1 up
+        -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0,    // v1-v6-v7-v2 left
+        -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,    // v7-v4-v3-v2 down
+        1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0     // v4-v7-v6-v5 back
     ]);
     //纹理坐标
     const texCoords = new Float32Array([
-        1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,  // v0-v1-v2-v3 
+        1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,    // v0-v1-v2-v3 front
         0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,    // v0-v3-v4-v5 right
         1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0,    // v0-v5-v6-v1 up
         1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,    // v1-v6-v7-v2 left
         0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,    // v7-v4-v3-v2 down
-        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0   // v4-v7-v6-v5
+        0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0     // v4-v7-v6-v5 back
     ]);
 
-    const indices = new Float32Array([
+    const indices = new Uint8Array([
         0, 1, 2, 0, 2, 3,      //front
         4, 5, 6, 4, 6, 7,      // right
         8, 9, 10, 8, 10, 11,   // up
@@ -245,7 +268,7 @@ function createVertexBuffer(gl) {
     if (!verticesBufferCreated || !texCoordsBufferCreated) {
         return -1;
     }
-    //TODO:
+
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     //将顶点数据写入缓冲区对象。
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
