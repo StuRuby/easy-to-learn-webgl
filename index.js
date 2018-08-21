383e70590a74b3a2566e8eeb255880165b5e195e
k <xingya.shi@gmail.com>
2018-08-21 13:49:45 +0800

    添加hud功能

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import page from 'page';
 import mouseControl from './apps/mouseControl';
 import pickedObject from './apps/picked/picked-object';
 import pickedFace from './apps/picked/picked-face';
+import hud from './apps/hud';
 
 page('/', function(){
-    pickedFace();
+    hud();
 });
 page();
